0545eef363763fb06e04c41d21cecd46e2c91f64
MJ <mwjang628@gmail.com>
2024-03-11 12:54:41 +1100

    http -> https

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -13,7 +13,7 @@ export default function VideoDetail() {
     <section className='flex flex-col lg:flex-row'>
       <article className='basis-4/6 '>
         <iframe id="player" type="text/html" width="100%" height="640"
-          src={`http://www.youtube.com/embed/${video.id}`}
+          src={`https://www.youtube.com/embed/${video.id}`}
           frameborder="0" title={title} />
         <div className='p-8'>
           <h2 className='text-xl font-bold'>{title}</h2>
